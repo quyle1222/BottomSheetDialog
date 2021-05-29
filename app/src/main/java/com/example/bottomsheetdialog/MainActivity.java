3193ced4517ff6fa56fc7f3e4bfa93e9db56c202
quyle1222 <thanhtu.vn.1999@gmail.com>
2021-05-30 00:29:23 +0700

    final

---
+++ app/src/main/java/com/example/bottomsheetdialog/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.bottomsheetdialog;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView filter;
+    RelativeLayout layoutFood;
+    LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        filter = findViewById(R.id.filterIcon);
+        filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this, R.style.BottomSheetDialogTheme);
+                View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.layout_bottom_sheet,findViewById(R.id.bottomSheetContainer));
+                bottomSheetDialog.setContentView(view);
+                bottomSheetDialog.show();
+            }
+        });
+        layoutFood = findViewById(R.id.layoutFood);
+        layoutFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().add(R.id.mainActivityContainer,new FragmentDetails()).commit();
+                linearLayout = findViewById(R.id.layoutActivity);
+                linearLayout.setVisibility(View.GONE);
+            }
+        });
+    }
+}
