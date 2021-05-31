962e0393b20edfc0d4f7cce947fd333cc973ccfb
quyle1222 <thanhtu.vn.1999@gmail.com>
2021-05-31 08:36:24 +0700

    add btnBackground

--- app/src/main/java/com/example/bottomsheetdialog/FragmentDetails.java
+++ app/src/main/java/com/example/bottomsheetdialog/FragmentDetails.java
@@ -1,12 +1,11 @@
 package com.example.bottomsheetdialog;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,15 +27,6 @@ public class FragmentDetails extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentDetails.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FragmentDetails newInstance(String param1, String param2) {
         FragmentDetails fragment = new FragmentDetails();
         Bundle args = new Bundle();
@@ -58,7 +48,9 @@ public class FragmentDetails extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_details, container, false);
     }
+
+
 }
